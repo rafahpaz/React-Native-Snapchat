a47c68975bfe483e0c5cd57f42d14505526f1f69
Rafael Paz <99267530+rafahpaz@users.noreply.github.com>
2023-04-24 10:05:32 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require('./snap.png')} style={styles.image} />
+      <TouchableOpacity style={[styles.button, { backgroundColor: '#f23c54', position: 'absolute', bottom: 130, alignSelf: 'center', marginTop: 20 }]}>
+        <Text style={[styles.buttonText, { textTransform: 'uppercase', textAlign: 'center' }]}>Entrar</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.button, { backgroundColor: '#0dabfa', position: 'absolute', bottom: 0, alignSelf: 'center', marginTop: 20 }]}>
+        <Text style={[styles.buttonText, { textTransform: 'uppercase', textAlign: 'center' }]}>Criar conta</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +18,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fffc00',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 400,
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#fff',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+    color: '#fff',
+  },
+  button: {
+    backgroundColor: '#f23c54',
+    padding: 25,
+    width: '100%',
+    height: '20%'
+  },
+  buttonText: {
+    color: '#e8eff2',
+    fontSize: 60,
   },
 });
